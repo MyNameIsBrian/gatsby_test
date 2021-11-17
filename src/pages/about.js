c5383e90826eef6ec74b87d9517877fe2a0f29ee
MyNameIsBrian <brian@sgnm.nl>
2021-11-17 23:00:27 +0100

    Gatsby tut p.2 afgerond

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+
+
+const AboutPage = () => {
+    return  (
+        <main>
+        <Layout pageTitle="About">
+          <p>Dit is de about pagina</p>
+        </Layout>
+      </main>
+    )
+}
+
+export default AboutPage
